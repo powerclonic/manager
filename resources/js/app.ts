447303603604c15f7cac5738f95440a1ec2387c8
Matheus Dresch <matheus_dresch@sicredi.com.br>
2024-09-05 16:33:48 -0300

    Initial commit

---
+++ resources/js/app.ts
@@ -0,0 +1,47 @@
+import "./bootstrap";
+import "@mdi/font/css/materialdesignicons.css";
+
+import { createApp, h, DefineComponent } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+});
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue")
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: "#4B5563",
+    },
+});
